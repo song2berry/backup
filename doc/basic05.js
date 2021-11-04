9db3a36cd659fbc104e5487b6bfa83e3aa143e14
Song E <91935099+song2berry@users.noreply.github.com>
2021-11-04 15:41:17 +0900

    Add files via upload

--- doc/basic05.js
+++ doc/basic05.js
@@ -1,16 +1,65 @@
 $(function(){
-//---------------------------------------
-$('#top_banner i').on('click', function(){
-    $('#top_banner').slideUp(); //slideUp(); hide();
-});
-
-$('.main_slider').slick({
-    arrows:false,
-    autoplay:true,
-    autoplaySpeed:2000,
-    pauseOnHover:false,
-    pauseOnFocus:false,
-});
-
-//---------------------------------------
-});
+    //-----------------------------------------------------------
+    $('#top_banner i').on('click', function(){
+        //$('#top_banner').hide();
+        $('#top_banner').slideUp();
+    });
+    
+    $('.main_slider').slick({
+        arrows:false,
+        autoplay:true,
+        autoplaySpeed:2000,
+        pauseOnHover:false,
+        pauseOnFocus:false,
+    });
+    
+    $('.pr_slider').slick({
+        arrows:false,
+        slidesToShow:5,
+        autoplay:true,
+        autoplaySpeed:2500,
+        //centerMode:true,
+        //centerPadding:"100px",
+        pauseOnHover:false,
+        pauseOnFocus:false,
+    });
+    $('.pr_slider figure').eq(7).addClass('on');
+    $('.pr_slider').on('afterChange', function(e,s,c){
+        // c = 1,2.....
+        $('.pr_slider figure').eq(c+7).addClass('on').siblings().removeClass('on');
+        $('#product03 .slide_bar>span').css({left:c*100})
+    })
+    
+    $('.pic').slick({
+        arrows:false,
+        autoplay:true,
+        pauseOnHover:false,
+        pauseOnFocus:false,
+        asNavFor:'.pic',
+    });
+    
+    $('.pr_slider02').slick({
+        arrows:false,
+        slidesToShow:3,
+        autoplay:true,
+        autoplaySpeed:3500,
+        //centerMode:true,
+        //centerPadding:"100px",
+        pauseOnHover:false,
+        pauseOnFocus:false,  
+    });
+    
+    $('#product04 i.xi-arrow-left').on('click', function(){
+        $('.pr_slider02').slick('slickPrev');
+    });
+    $('#product04 i.xi-arrow-right').on('click', function(){
+        $('.pr_slider02').slick('slickNext');
+    });
+    
+    $('#link select').on('change', function(){
+        var link=$(this).val();
+        if(link) window.open(link)
+    });
+    
+    //-----------------------------------------------------------
+    });
