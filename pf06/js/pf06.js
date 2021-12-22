67d4352ade69a541525a4ad5eb0a6b283c21473e
Song E <91935099+song2berry@users.noreply.github.com>
2021-12-22 17:54:54 +0900

    Add files via upload

--- pf06/js/pf06.js
+++ pf06/js/pf06.js
@@ -8,7 +8,18 @@ $(function(){
     $('.con01_slider').slick({
         arrows:false,
         autoplay:true,
+        autoplaySpeed:2000,
+        pauseOnhover:false,
+        pauseOnFocus:false,
+        infinite:true,
     });
+
+    $('#content01 .container .box1 i.xi-angle-left').on('click',function(){
+        $('.con01_slider').slick('slickPrev');
+        });
+        $('#content01 .container .box1 i.xi-angle-right').on('click',function(){
+            $('.con01_slider').slick('slickNext');
+        });
 
     $('.gnb>ul>li>a').on('click', function(e){
         if($('.gnb').hasClass('on')){
