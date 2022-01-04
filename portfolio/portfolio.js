81e36d3241c231a2361c2ed3367f71e7f6bb1a42
Song E <91935099+song2berry@users.noreply.github.com>
2022-01-04 16:35:33 +0900

    Add files via upload

---
+++ portfolio/portfolio.js
@@ -0,0 +1,22 @@
+$(function(){
+    //-----------------------------------------------------
+    
+    let sc=$('.section');
+    let sideBar=$('nav li');
+    $('#main').fullpage({
+        anchors:['intro', 'portfolio_01', 'portfolio_02', 'portfolio_03', 'portfolio_04', 'portfolio_05', 'profile'],
+        afterLoad:function(origin, destination, direction){
+            let idx=destination.index;
+            sc.eq(idx).addClass('on').siblings().removeClass('on');
+            sideBar.eq(idx).addClass('on').siblings().removeClass('on');
+        },
+    });
+    
+    new Typed('.slogan_txt',{
+        strings: ['안녕하세요', '제목입니다.'],
+        typeSpeed:100,
+    });
+    
+    
+    //-----------------------------------------------------
+    })
