0d836b062408f257f632a47ed41dc5c68157e480
Song E <91935099+song2berry@users.noreply.github.com>
2022-01-04 19:05:35 +0900

    Add files via upload

--- pf07/js/pf07.js
+++ pf07/js/pf07.js
@@ -13,8 +13,16 @@ $(function(){
 
     $('.mopen').on('click', function(){
         $('.gnb').toggleClass('on');
+        $(this).toggleClass('on');
     });
-    
+
+    $('.gnb>ul>li>a').on('click', function(){
+        if($(window).width() < 769) {
+            $(this).next().slideToggle();
+            $(this).parent().siblings().find('.submenu').slideUp();
+        }
+    });
+
     $(window).on('resize', function(){
         $('.submenu').removeAttr('style')
     });
@@ -111,7 +119,5 @@ $(function(){
     });
 
     
-
-
     //------------------------------------------------------
     });
