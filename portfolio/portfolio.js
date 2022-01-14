b57e0947482da8a7f180810baec3dfd1c9b8f465
Song E <91935099+song2berry@users.noreply.github.com>
2022-01-14 14:30:57 +0900

    Add files via upload

--- portfolio/portfolio.js
+++ portfolio/portfolio.js
@@ -4,19 +4,49 @@ $(function(){
     let sc=$('.section');
     let sideBar=$('nav li');
     $('#main').fullpage({
-        anchors:['intro', 'portfolio_01', 'portfolio_02', 'portfolio_03', 'portfolio_04', 'portfolio_05', 'profile'],
-        afterLoad:function(origin, destination, direction){
+        anchors:['intro', 'Portfolio_01', 'Portfolio_02', 'Portfolio_03', 'Portfolio_04', 'Portfolio_05', 'Profile', 'Footer'],
+        afterLoad:function(origin, destination, direction)
+        {
             let idx=destination.index;
             sc.eq(idx).addClass('on').siblings().removeClass('on');
             sideBar.eq(idx).addClass('on').siblings().removeClass('on');
         },
     });
     
-    new Typed('.slogan_txt',{
-        strings: ['안녕하세요', '제목입니다.'],
-        typeSpeed:100,
-    });
     
+
+    new Typed('.slogan_txt',{
+        strings: ['Welcome to my portfolio!'],
+        typeSpeed:150,
+    });
+
+
+    //$('.cover_btn').on('click', function(){
+    //    $(this).toggleClass('on');
+    //    $('#Cover').slideToggle();
+
+    //});
+
+    // 버튼 변경
+    $('.cover_btn').on('click', function(){
+        $(this).toggleClass('on');
+
+        if($('#Cover').slideToggle()) {
+      }
+    })
+    
+    let cloneMenu = $('nav>ul').clone();
+    $('#Cover').append(cloneMenu);
+
+    $('#Cover a').on('click', function(){
+        $('#Cover').slideToggle();
+        $('.cover_btn').toggleClass('on');
+    });
+
+
+    $('#Cover').on('scroll wheel touchmove', function(){
+        return false;
+    });
     
     //-----------------------------------------------------
     })
